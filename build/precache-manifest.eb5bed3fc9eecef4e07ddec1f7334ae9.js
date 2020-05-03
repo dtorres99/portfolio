6f883441973d74fa0a33f1379828facf55623a32
Daniel Torres <disidrot@gmail.com>
2020-05-03 06:50:26 -0700

    Added build directory to host website from

---
+++ build/precache-manifest.eb5bed3fc9eecef4e07ddec1f7334ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55edd4d80cf61f0802de706bbc4fba8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bb803f13011531a181ed",
+    "url": "/portfolio/static/css/main.9cc4679a.chunk.css"
+  },
+  {
+    "revision": "531f42552d32a0a2628a",
+    "url": "/portfolio/static/js/2.d979e6f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.d979e6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb803f13011531a181ed",
+    "url": "/portfolio/static/js/main.ba8ed8d1.chunk.js"
+  },
+  {
+    "revision": "77ca489b103bedac9a32",
+    "url": "/portfolio/static/js/runtime-main.7be96b21.js"
+  }
+]);
